04b4748e80a84506524e9db1c40ceb6da329d39b
TylerCharlton <TyCharlton1993@gmail.com>
2023-12-04 05:31:36 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const mainElement = document.querySelector('main#main');
+mainElement.remove();
+
+const headerElement = document.createElement('h1');
+headerElement.textContent = 'YOUR-NAME is the champion';
+headerElement.id = 'victory';
+document.body.appendChild(headerElement);
+
+const newHeader = headerElement;
